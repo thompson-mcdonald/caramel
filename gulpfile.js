bd6e9242b7c0942b404eea7548d0f73481288d50
jacob thompson <thampsen@gmail.com>
2016-06-28 21:22:23 +0100

    initial commit of project

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var gulp = require('gulp'),
+		connect = require('gulp-connect'),
+		postcss = require('gulp-postcss');
+
+var postcssPlugins = [
+	require('postcss-nested')
+];
+
+gulp.task('styles', function(){
+	return gulp.src('src/styles/style.css')
+	.pipe(postcss(postcssPlugins))
+	.pipe(gulp.dest('dest/styles'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/styles/**/*.css', ['styles']);
+});
+
+gulp.task('connect', function(){
+	connect.server({
+		root: './'
+	});
+});
+
+gulp.task('default', ['connect', 'watch', 'styles'])
